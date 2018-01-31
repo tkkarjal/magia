400b996e61fa53342b739fc095ece63c45a8fe56
Tomi Karjalainen <tomi.karjalainen@aalto.fi>
2018-01-31 13:42:30 +0200

    trilinear interpolation
    replace 4th order spline interpolation with simple trilinear interpolation

--- normalize_using_mri.m
+++ normalize_using_mri.m
@@ -15,7 +15,7 @@ matlabbatch{1}.spm.spatial.normalise.write.subj.resample = resampled_images;
 matlabbatch{1}.spm.spatial.normalise.write.woptions.bb = [-78 -112 -70
                                                           78 76 85];
 matlabbatch{1}.spm.spatial.normalise.write.woptions.vox = [1 1 1];
-matlabbatch{1}.spm.spatial.normalise.write.woptions.interp = 4;
+matlabbatch{1}.spm.spatial.normalise.write.woptions.interp = 1;
 matlabbatch{1}.spm.spatial.normalise.write.woptions.prefix = 'w';
 
 normalized_images = add_prefix(resampled_images,'w');
