8d5d0d5966437686381864437fe193fb2b646163
Tomi Karjalainen <tomi.karjalainen@aalto.fi>
2018-02-02 10:43:17 +0200

    save mask_dir into roi_info

---
+++ magia_get_roi_info.m
@@ -0,0 +1,17 @@
+function roi_info = magia_get_roi_info(roi_set,tracer)
+
+if(strcmpi(roi_set,'tracer_default'))
+    roi_info = get_tracer_default_roi_set(tracer);
+elseif(strcmpi(roi_set,'atlas'))
+    mask_dir = '/scratch/shared/megapet/masks';
+    roi_info = get_atlas_rois(mask_dir);
+    roi_info.mask_dir = mask_dir;
+elseif(strcmpi(roi_set,'[18f]fdg_atlas'))
+    mask_dir = '/scratch/shared/megapet/fdg_rois';
+    roi_info = get_atlas_rois(mask_dir);
+    roi_info.mask_dir = mask_dir;
+else
+    roi_info = read_roi_info(roi_set);
+end
+
+end
