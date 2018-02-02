74e42b083f6f6f451beefb568b083405837f26d5
Tomi Karjalainen <tomi.karjalainen@aalto.fi>
2018-02-02 10:44:24 +0200

    simplify magia structure

---
+++ magia_get_ref_region.m
@@ -0,0 +1,12 @@
+function ref_region = magia_get_ref_region(tracer)
+
+switch tracer
+    case {'[11c]carfentanil' '[18f]dopa'}
+        ref_region.label = 'OC';
+        ref_region.codes = [1011 2011];
+    case {'[11c]raclopride','[11c]madam','[18f]spa-rq','[11c]pib','[11c]pbr28','[18f]cft'}
+        ref_region.label = 'CER';
+        ref_region.codes = [8 47];
+end
+
+end
