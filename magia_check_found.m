bee1f3ed0e4e54018c70ec4f2d0968de3b24838d
Tomi Karjalainen <tomi.karjalainen@aalto.fi>
2018-03-20 19:20:58 +0200

    magia_check_found also detects nifti-only cases

--- magia_check_found.m
+++ magia_check_found.m
@@ -4,33 +4,42 @@ data_path = getenv('DATA_DIR');
 sub_dir = sprintf('%s/%s',data_path,image_id);
 if(~exist(sub_dir,'dir'))
     found = 0;
+    return;
 else
     pet_dir = sprintf('%s/PET',sub_dir);
+    nii_dir = sprintf('%s/nii',pet_dir);
     dcm_dir = sprintf('%s/dcm',pet_dir);
-    if(~exist(dcm_dir,'dir'))
-        ecat_dir = sprintf('%s/ecat',pet_dir);
-        if(~exist(ecat_dir,'dir'))
-            found = 0;
+    ecat_dir = sprintf('%s/ecat',pet_dir);
+    if(exist(nii_dir,'dir'))
+        f = get_filenames(nii_dir,'*.nii');
+        if(~isempty(f))
+            found = 1;
+            return;
+        end
+    end
+    if(exist(dcm_dir,'dir'))
+        f = get_filenames(dcm_dir,'*.dcm');
+        if(~isempty(f))
+            found = 1;
+            return;
+        end
+    end
+    if(exist(ecat_dir,'dir'))
+        f = get_filenames(ecat_dir,'*.img');
+        if(~isempty(f))
+            found = 1;
+            return;
         else
             f = get_filenames(ecat_dir,'*.v');
-            if(isempty(f))
-                f = get_filenames(ecat_dir,'*.img');
-                if(isempty(f))
-                    found = 0;
-                else
-                    found = 1;
-                end
-            else
+            if(~isempty(f))
                 found = 1;
+                return;
+            else
+                found = 0;
             end
         end
     else
-        f = get_filenames(dcm_dir,'*.dcm');
-        if(isempty(f))
-            found = 0;
-        else
-            found = 1;
-        end
+        found = 0;
     end
 end
 
