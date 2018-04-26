d0de9764dbbc60a641d2b1acb88a8e6cb27a40a1
Tomi Karjalainen <tomi.karjalainen@aalto.fi>
2018-04-26 15:16:43 +0300

    allow char input in magia_medfilter

--- magia_medfilter.m
+++ magia_medfilter.m
@@ -1,5 +1,11 @@
 function new_image = magia_medfilter(old_image)
 
+if(ischar(old_image))
+    V = spm_vol(old_image);
+    old_image = spm_read_vols(V);
+    clear V
+end
+
 new_image = old_image;
 for z = 1:size(old_image,3)
     old_slice = old_image(:,:,z);
