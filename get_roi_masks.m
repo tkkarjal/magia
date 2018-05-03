bf291fb4b41d3a5c8fe93d67df5365886e6878e5
Tomi Karjalainen <tomi.karjalainen@aalto.fi>
2018-05-03 16:03:19 +0300

    Decided to keep the orig refregion among roi_masks

--- get_roi_masks.m
+++ get_roi_masks.m
@@ -6,7 +6,7 @@ if(nargin==2)
     ref_region = varargin{1};
     ref_idx = find(1-cellfun(@isempty,regexp(roi_masks,ref_region)));
     ref_mask = roi_masks{ref_idx};
-    roi_masks(ref_idx) = [];
+    % roi_masks(ref_idx) = [];
     varargout{1} = roi_masks;   
     varargout{2} = ref_mask;
 end
