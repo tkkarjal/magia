39b5ada55cb8fc4726a815668e74ca3a020dbd32
Tomi Karjalainen <tomi.karjalainen@aalto.fi>
2018-12-17 00:11:15 +0200

    bug fix
    reference-tissue modeling with atlas ROIs resulted in error

--- get_roi_masks.m
+++ get_roi_masks.m
@@ -8,8 +8,13 @@ if(nargin > 1)
     subject_mask_dir = sprintf('%s/%s/masks',data_path,subject);
     mkdir(subject_mask_dir);
     if(nargin > 2)
-        ref_idx = find(1-cellfun(@isempty,regexp(roi_masks,ref_region)));
+        ref_region = varargin{2};
+        ref_idx = ~cellfun(@isempty,regexp(roi_masks,ref_region));
         ref_mask = roi_masks{ref_idx};
+        roi_masks(ref_idx) = [];
+        copyfile(ref_mask,subject_mask_dir,'f')
+        [~,n] = fileparts(ref_mask);
+        ref_mask = fullfile(subject_mask_dir,[n '.nii']);
         varargout{2} = ref_mask;
     end
     for j = 1:length(roi_masks)
