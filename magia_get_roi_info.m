5522a24886ef926bb8341f04e583bbca8f19ca57
Tomi Karjalainen <tomi.karjalainen@aalto.fi>
2019-03-20 14:09:59 +0200

    Update magia_get_roi_info.m

--- magia_get_roi_info.m
+++ magia_get_roi_info.m
@@ -10,6 +10,10 @@ elseif(strcmpi(roi_set,'[18f]fdg_atlas'))
     mask_dir = '/scratch/shared/megapet/fdg_rois';
     roi_info = get_atlas_rois(mask_dir);
     roi_info.mask_dir = mask_dir;
+elseif(strcmpi(roi_set,'bug_12_roi'))
+    mask_dir = '/scratch/shared/megapet/bug_12_roi';
+    roi_info = get_atlas_rois(mask_dir);
+    roi_info.mask_dir = mask_dir;
 elseif(strcmpi(roi_set,'[11c]raclopride_atlas'))
     mask_dir = '/scratch/shared/megapet/raclo_rois';
     roi_info = get_atlas_rois(mask_dir);
