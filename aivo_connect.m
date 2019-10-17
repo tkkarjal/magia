20abf542e7edb9fb207161a3c491995329f50302
Tomi Karjalainen <tomi.karjalainen@utu.fi>
2019-10-17 10:21:05 +0300

    Delete aivo_connect.m

--- aivo_connect.m
+++
@@ -1,8 +0,0 @@
-function conn = aivo_connect()
-
-conn = check_megabase_conn();
-if(isempty(conn))
-    conn = database('megabase', 'malmsteen', ' ', 'Vendor', 'POSTGRESQL', 'Server', 'localhost', 'PortNumber', 5432);
-end
-
-end
