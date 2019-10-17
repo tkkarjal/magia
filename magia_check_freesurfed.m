9f9056491f98846e01085267dbd2c3c2aea7e669
Tomi Karjalainen <tomi.karjalainen@utu.fi>
2019-10-17 10:57:01 +0300

    Change in magia_check_freesurfed
    Magia used to check if the error file exists, and then decide the freesurfer outputs were not available. That does not seem to be the case.

--- magia_check_freesurfed.m
+++ magia_check_freesurfed.m
@@ -5,16 +5,11 @@ sub_dir = sprintf('%s/%s',fs_dir,mri_code);
 if(~exist(sub_dir,'dir'))
     freesurfed = 0;
 else
-    error_file = sprintf('%s/scripts/recon-all.error',sub_dir);
-    if(exist(error_file,'file'))
-        freesurfed = -1;
+    done_file = sprintf('%s/scripts/recon-all.done',sub_dir);
+    if(exist(done_file,'file'))
+       freesurfed = 1;
     else
-        done_file = sprintf('%s/scripts/recon-all.done',sub_dir);
-        if(exist(done_file,'file'))
-            freesurfed = 1;
-        else
-            freesurfed = 0;
-        end
+       freesurfed = 0;
     end
 end
 
