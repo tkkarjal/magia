d339dd5843e1faa416aa89addc5c7298bfcede3d
Tomi Karjalainen <tomi.karjalainen@utu.fi>
2019-10-28 12:13:14 +0200

    bug fix

--- magia_get_mri_file.m
+++ magia_get_mri_file.m
@@ -7,16 +7,13 @@ t1_folder = sprintf('%s/%s/T1',mri_dir,mri_code);
 cmd = sprintf('gunzip -rf %s',t1_folder);
 system(cmd);
 
-mri_filename = sprintf('mri_%s.nii',pet_code);
-convert_to_nifti(t1_folder,t1_folder,mri_filename);
-mri_file = sprintf('%s/%s',t1_folder,mri_filename);
-
 target_dir = sprintf('%s/%s/MRI',data_dir,pet_code);
 if(~exist(target_dir,'dir'))
     mkdir(target_dir);
 end
 
-movefile(mri_file,target_dir);
+mri_filename = sprintf('mri_%s.nii',pet_code);
+convert_to_nifti(t1_folder,target_dir,mri_filename);
 mri_file = sprintf('%s/mri_%s.nii',target_dir,pet_code);
 
 center_image(mri_file);
