39f4f722d1d705ea3d2297a39ef8eb65cf5cc21f
Tomi Karjalainen <tomi.karjalainen@utu.fi>
2019-11-04 09:19:27 +0200

    Update magia_read_specs.m

--- magia_read_specs.m
+++ magia_read_specs.m
@@ -9,8 +9,16 @@ while(l ~= -1)
     value = l(split_idx+2:end);
     switch field_name
         case {'tracer' 'frames' 'weight' 'dose' 'scanner' 'mri_code'}
+            if(strcmp(field_name,'tracer'))
+                value = parse_frames_string(value);
+            elseif(strcmp(field_name,'dose'))
+                value = str2double(value);
+            end
             specs.study.(field_name) = value;
         otherwise
+            if(ismember(field_name,{'cpi','dc','rc','fwhm_pre','fwhm_post','fwhm_roi','cut_time','mc_fwhm','mc_rtm','mc_ref_frame'}))
+                value = str2double(value);
+            end
             specs.magia.(field_name) = value;
     end
     l = fgetl(fid);
