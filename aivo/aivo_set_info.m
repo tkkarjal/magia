08376045bdab41ebf68f7c117a2254f039a8c4b8
Tomi Karjalainen <tomi.karjalainen@utu.fi>
2019-11-06 14:20:11 +0200

    Add files via upload

--- aivo/aivo_set_info.m
+++ aivo/aivo_set_info.m
@@ -1,111 +1,76 @@
 function aivo_set_info(subject_id,field,value)
-% It is possible to add the same value for every subject_id. Name the fields as in the columns in aivo.pet.
-% Do not enter many fields.
+% It is possible to add the same value for every subject_id. Only one field
+% is accepted. Note that if many subject_ids are given, they must be given
+% in alphabetical order.
 
 conn = aivo_connect();
 
-switch field
-    case {'study_date' 'tracer' 'scanner' 'mri_code' 'frames' 'dose' 'plasma' 'num_frames' 'scan_start_time' 'injection_time' 'type' 'source' 'dynamic'}
-        table_name = '"megabase"."aivo2".study';
-        cols = columns(conn,'megapet','aivo2','study');
-        field_edits_allowed = 1;
-    case {'model' 'dc' 'rc' 'cpi' 'cut_time' 'roi_type' 'roi_set' 'mni_roi_atlas_dir' 'input_type' 'fwhm_pre' 'fwhm_post' 'norm_method' 'fwhm_roi' 'mc_excluded_frames' 'mc_fwhm' 'mc_rtm' 'mc_ref_frame' 'template'}
-        table_name = '"megabase"."aivo2".magia';
-        cols = columns(conn,'megapet','aivo2','magia');
-        field_edits_allowed = 1;
-    case {'patient_id' 'weight' 'height' 'age' 'sex' 'birthday' 'handedness' 'smoker'}
-        table_name = '"megabase"."aivo2".patient';
-        cols = columns(conn,'megapet','aivo2','patient');
-        field_edits_allowed = 1;
-    case {'found' 'freesurfed' 'analyzed' 'magia_time' 'magia_githash' 'error'}
-        table_name = '"megabase"."aivo2".inventory';
-        cols = columns(conn,'megapet','aivo2','inventory');
-        field_edits_allowed = 1;
-    otherwise
-        error('Unknown field %s.',field);
+study_cols = columns(conn,'megapet','aivo2','study');
+patient_cols = columns(conn,'megapet','aivo2','patient');
+magia_cols = columns(conn,'megapet','aivo2','magia');
+lab_cols = columns(conn,'megapet','aivo2','lab');
+inventory_cols = columns(conn,'megapet','aivo2','inventory');
+mri_cols = {'freesurfed'};
+
+if(ismember(field,study_cols))
+    table_name = '"megabase"."aivo2".study';
+elseif(ismember(field,magia_cols))
+    table_name = '"megabase"."aivo2".magia';
+elseif(ismember(field,patient_cols))
+    table_name = '"megabase"."aivo2".patient';
+elseif(ismember(field,inventory_cols))
+    table_name = '"megabase"."aivo2".inventory';
+elseif(ismember(field,lab_cols))
+    table_name = '"megabase"."aivo2".lab';
+elseif(ismember(field,mri_cols))
+    table_name = '"megabase"."aivo2".mri';
+else
+    error('Could not find the field ''%s'' from AIVO',field);
 end
 
-if(~ismember(field,cols))
-    error('You have entered an invalid field!');
-end;
-
 if(~iscell(value))
-    value = {value};
+    if(isnumeric(value))
+        value = num2cell(value);
+    else
+        value = {value};
+    end
 end
 if(~iscell(field))
     field = {field};
 end
-
-if(~ischar(subject_id)) %many subjects 
-    if(length(subject_id) == length(value)) %different value for every subject
-        for i=1:length(subject_id)
-            validated = aivo_get_info(subject_id{i},'validated');
-            if(validated ~= 1 || field_edits_allowed)
-                whereclause = sprintf('WHERE image_id = %s%s%s',char(39),subject_id{i},char(39));
-                update(conn,table_name,field,value(i),whereclause)
-            else
-                warning('Subject_id %s is validated and cannot be updated!',subject_id{i});
-            end
-        end
-        return
-    else
-        if(length(value)>1)
-            error('You have entered a different number of subject_id:s and values. If you want to add different values for different subject_id:s you must specify one value for every subject_id.');
-        end
-    end
+if(~iscell(subject_id))
+    subject_id = {subject_id};
 end
 
-current_subjects = aivo_get_subjects;
-invalid_id = setdiff(subject_id,current_subjects); 
-subject_id = intersect(subject_id,current_subjects); %exclude invalid subjects
-for i=1:length(invalid_id)
-    warning('Image_id %s does not exist! Use aivo_create_subject.m to create new subjects.',invalid_id{i})
+sorted_subject_id = sort(subject_id);
+if(~isequal(sorted_subject_id,subject_id))
+    error('The subject IDs were not given in alphabetical order. Please make sure the subjects are given in alphabetical order, make sure that the inserted values then follow the same order, and try again.');
 end
 
-if ~field_edits_allowed
-    current_validated = aivo_get_subjects('validated',1);
-    current_unvalidated = setdiff(current_subjects,current_validated);
-    if(~isempty(current_unvalidated)) %exclude validated subjects
-        validated = intersect(subject_id,current_validated);
-        subject_id = intersect(subject_id,current_unvalidated);
-        for i=1:length(validated)
-            warning('Subject_id %s is validated and cannot be updated!',validated{i});
-        end
-    end
-    if(isempty(subject_id)) %end if no subjects
-        return
-    end
-end
-    
-if(length(subject_id)>1) %many subjects
-    if(length(subject_id) == length(value)) %different value for every subject (not working at the moment!)
-        %for i=1:length(subject_id)
-            %whereclause = sprintf('WHERE image_id = %s%s%s',char(39),subject_id{i},char(39));
-            %update(conn,'"megabase"."aivo2".pet',field,value(i),whereclause)
-        %end
-    else
-        if(length(value)) %same value for all subjects
-            whereclause = 'WHERE';
-            for i=1:length(subject_id)
-                if(i==1)
-                    whereclause = sprintf('%s image_id = %s%s%s',whereclause,char(39),subject_id{i},char(39));
-                else
-                    whereclause = sprintf('%s OR image_id = %s%s%s',whereclause,char(39),subject_id{i},char(39));
-                end
-            end
-            update(conn,table_name,field,value,whereclause)
+n_subs = length(subject_id);
+n_values = length(value);
+
+if(n_values == 1 && n_subs >= 1) % A single value for all subjects
+    whereclause = 'WHERE';
+    for i = 1:n_subs
+        if(i == 1)
+            whereclause = sprintf('%s image_id = ''%s''',whereclause,subject_id{i});
         else
-            error('You have entered a different number of subject_id:s and values. If you want to add different values for different subject_id:s you must specify one value for every subject_id.');
+            whereclause = sprintf('%s OR image_id = ''%s''',whereclause,subject_id{i});
         end
     end
-else %one subject
-    if(length(value)==1) %one value
-        table_name_only=strsplit(table_name,'.');
-        whereclause = sprintf('WHERE %s.image_id = %s%s%s',table_name_only{3},char(39),subject_id{1},char(39));
-        update(conn,table_name,field,value,whereclause)
+    update(conn,table_name,field,value,whereclause)
+elseif(n_values > 1 && n_subs > 1) % Subjects have a distinct value
+    if(n_values == n_subs)
+        for i = 1:n_subs
+            whereclause = sprintf('WHERE image_id = ''%s''',subject_id{i});
+            update(conn,table_name,field,value(i),whereclause)
+        end
     else
-        error('You have entered a different number of subject_id:s and values. If you want to add different values for different subject_id:s you must specify one value for every subject_id.');
+        error('The number of values does not match the number of subjects.');
     end
+else
+    error('Inconsistent combination of values and subjects. The function can either use a single value for all subjects, or a distinct value per subject.');
 end
 
 close(conn);
--- aivo/aivo_store_magia_info.m
+++ aivo/aivo_store_magia_info.m
@@ -1,4 +1,4 @@
-function aivo_store_magia_info(subject)
+function aivo_store_magia_info(subject,specs)
 
 aivo_set_info(subject,'magia_time',char(datetime('now','format','yyyy-MM-dd hh:mm:ss')));
 aivo_set_info(subject,'found',1);
@@ -6,11 +6,13 @@ aivo_set_info(subject,'analyzed',1);
 aivo_set_info(subject,'error','');
 aivo_set_info(subject,'magia_githash',magia_get_githash);
 
-mri_code = aivo_get_info(subject,'mri_code');
-if(iscell(mri_code))
-    mri_code = mri_code{1};
+if(strcmp(specs.magia.roi_type,'freesurfer'))
+    mri_code = specs.study.mri_code;
+    aivo_set_info(mri_code,'freesurfed',1);
+    aivo_set_info(mri_code,'found',1);
+elseif(strcmp(specs.magia.norm_method,'mri'))
+    mri_code = specs.study.mri_code;
+    aivo_set_info(mri_code,'found',1);
 end
-freesurfed = magia_check_freesurfed(mri_code);
-aivo_set_info(subject,'freesurfed',freesurfed);
 
 end
