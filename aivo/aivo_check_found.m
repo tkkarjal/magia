a307f2e06a92bd3e63e730efcdc803dddea08172
Tomi Karjalainen <tomi.karjalainen@utu.fi>
2019-11-07 12:18:35 +0200

    Add files via upload

--- aivo/aivo_check_found.m
+++ aivo/aivo_check_found.m
@@ -1,6 +1,31 @@
-function found = aivo_check_found(image_id)
+function found = aivo_check_found(image_id,varargin)
 
-aivo_subjects = aivo_get_subjects();
-found = ismember(image_id,aivo_subjects);
+if(nargin == 1)
+    table_name = 'study';
+else
+    table_name = varargin{1};
+end
+
+if(ischar(image_id))
+    image_id = {image_id};
+end
+
+available_tables = {'study' 'magia' 'patient' 'lab' 'project' 'inventory' 'mri'};
+
+if(~ismember(table_name,available_tables))
+    error('Unrecognized table name: %s',table_name);
+end
+
+q = sprintf('SELECT %s.image_id FROM "megabase"."aivo2".%s ',table_name,table_name);
+
+conn = aivo_connect();
+curs = exec(conn,q);
+curs = fetch(curs);
+close(curs);
+close(conn);
+
+table_image_ids = curs.Data;
+
+found = ismember(image_id,table_image_ids);
 
 end
