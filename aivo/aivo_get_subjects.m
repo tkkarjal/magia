b08c3ad5c96f270727eb6db1015ce78bb2150759
Tomi Karjalainen <tomi.karjalainen@utu.fi>
2019-11-11 09:47:07 +0200

    Update aivo_get_subjects.m

--- aivo/aivo_get_subjects.m
+++ aivo/aivo_get_subjects.m
@@ -41,14 +41,14 @@ conn = aivo_connect();
 
 if((~mod(nargin,2)))
     
-    select_statement = 'SELECT study.image_id FROM "megabase"."aivo2".study, "megabase"."aivo2".patient , "megabase"."aivo2".inventory ';
-    where_statement = 'WHERE "aivo2".study.image_id = "aivo2".patient.image_id AND "aivo2".study.image_id = "aivo2".inventory.image_id AND ';
+    select_statement = 'SELECT study.image_id FROM "megabase"."aivo2".study, "megabase"."aivo2".patient , "megabase"."aivo2".inventory, "megabase"."aivo2".main ';
+    where_statement = 'WHERE "aivo2".study.image_id = "aivo2".patient.image_id AND "aivo2".study.image_id = "aivo2".inventory.image_id AND "aivo2".study.image_id = "aivo2".main.image_id AND ';
     l=0;
     for i=1:nargin/2
         field = varargin{i+l};
         value = varargin{i+l+1};
         l=l+1;
-        if(ismember(field,{'age','dose','study_date','injection_time'})) %value
+        if(ismember(field,{'study_date','tracer','mri_code','dose','scan_start_time','injection_time'})) %value
             if(ischar(value)) %only one value
                 if(ismember('~',value))  %exclude spesific value
                     where_statement = [where_statement,' NOT ','study.',lower(field),' = ',char(39),value(2:length(value)),char(39)];
@@ -69,7 +69,7 @@ if((~mod(nargin,2)))
                 where_statement = [where_statement,' AND '];
             end
         end
-        if(ismember(field,{'height','weight'})) %value
+        if(ismember(field,{'height','weight','age'})) %value
             if(ischar(value)) %only one value
                 if(ismember('~',value))  %exclude spesific value
                     where_statement = [where_statement,' NOT ','patient.',lower(field),' = ',char(39),value(2:length(value)),char(39)];
@@ -86,20 +86,7 @@ if((~mod(nargin,2)))
                 where_statement = [where_statement,' AND '];
             end
         end       
-        if(ismember(field,{'gender','study_code','tracer','frames','scanner','mri','project','description','group_name','patient_id','source','type','ac','githash'})) %char
-            if(ismember('~',value)) %user excludes spesified values
-                where_statement = [where_statement,' NOT ','study.',lower(field),'=',char(39),value(2:length(value)),char(39)];
-                if(i~=nargin/2)
-                    where_statement = [where_statement,' AND '];
-                end
-            else
-                where_statement = [where_statement,'study.',lower(field),'=',char(39),value,char(39)];
-                if(i~=nargin/2)
-                    where_statement = [where_statement,' AND '];
-                end
-            end
-        end
-        if(ismember(field,{'mri_found','rc','dc','plasma' 'dynamic' 'nii'})) %integer or char
+        if(ismember(field,{'frames','mri_code','scanner','tracer'})) %integer or char
             if(ismember('~',value)) %user excludes spesified values, value is char
                 where_statement = [where_statement,' NOT ','study.',lower(field),'=',char(39),value(2:length(value)),char(39)];
                 if(i~=nargin/2)
@@ -115,7 +102,7 @@ if((~mod(nargin,2)))
                 end
             end
         end 
-        if(ismember(field,{'analyzed','found','freesurfed'})) %integer or char
+        if(ismember(field,{'analyzed','found','freesurfed','magia_time','error','magia_githash'})) %integer or char
             if(ismember('~',value)) %user excludes spesified values, value is char
                 where_statement = [where_statement,' NOT ','inventory.',lower(field),' =',char(39),value(2:length(value)),char(39)];
                 if(i~=nargin/2)
@@ -126,6 +113,22 @@ if((~mod(nargin,2)))
                     value = num2str(value);
                 end
                 where_statement = [where_statement,'inventory.',lower(field),' =',char(39),value,char(39)];
+                if(i~=nargin/2)
+                    where_statement = [where_statement,' AND '];
+                end
+            end
+        end 
+        if(ismember(field,{'project','group_name','description','scanner','notes','type','source'})) %integer or char
+            if(ismember('~',value)) %user excludes spesified values, value is char
+                where_statement = [where_statement,' NOT ','main.',lower(field),' =',char(39),value(2:length(value)),char(39)];
+                if(i~=nargin/2)
+                    where_statement = [where_statement,' AND '];
+                end
+            else
+                if(isnumeric(value)) % value may be char or numeric
+                    value = num2str(value);
+                end
+                where_statement = [where_statement,'main.',lower(field),' =',char(39),value,char(39)];
                 if(i~=nargin/2)
                     where_statement = [where_statement,' AND '];
                 end
