b74623069022e65e261cdd9fd21be82b7fb18f9b
Tomi Karjalainen <tomi.karjalainen@utu.fi>
2019-11-25 11:07:13 +0200

    Add files via upload

--- magia_suv_image.m
+++ magia_suv_image.m
@@ -1,20 +1,23 @@
-function suv_img_file = magia_suv_image(pet_file,dose,weight,brainmask,outputdir)
+function suv_img_file = magia_suv_image(pet_file,dose,weight)
 
-V = spm_vol(pet_file);
-img = spm_read_vols(V);
-c = dose/weight;
-suv_img = img./c;
+[p,n] = fileparts(pet_file);
+files = cellstr(spm_select('ExtList',p,n));
+suv_factor = dose/weight;
 
-V = spm_vol(brainmask);
-mask = spm_read_vols(V);
-suv_img = suv_img.*double(logical(mask));
-
-[~,n] = fileparts(pet_file);
-
-suv_img_file = sprintf('%s/%s_suv.nii',outputdir,n);
-V.fname = suv_img_file;
-V.dt = [spm_type('int16') spm_platform('bigend')];
-V.pinfo = [Inf Inf Inf]';
-spm_write_vol(V,suv_img);
+n_frames = size(files,1);
+suv_img_file = sprintf('%s/%s_suv.nii',p,n);
+for i = 1:n_frames
+    file = files{i};
+    V = spm_vol(file);
+    img = spm_read_vols(V);
+    m = max(img(:));
+    if(m > 1000)
+        img = img.*0.001;
+    end
+    img2 = suv_factor.*img;
+    V.fname = suv_img_file;
+    V.n(1) = i;
+    spm_write_vol(V,img2);
+end
 
 end
