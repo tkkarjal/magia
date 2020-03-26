c270897fa752621c372e4cb04dddb87a282821b8
Tomi Karjalainen <tomi.karjalainen@utu.fi>
2020-03-26 09:05:58 +0200

    Added guardian for FUR integral calculation

--- calculate_fur_integral.m
+++ calculate_fur_integral.m
@@ -8,6 +8,10 @@ a = input(:,2);
 a = spline(t,a,tt);
 de = frames(1) + (frames(end)-frames(1))/2;
 idx = find(tt>de,1,'first');
-fur_integral = trapz(tt(1:idx),a(1:idx));
+if(~isempty(idx))
+    fur_integral = trapz(tt(1:idx),a(1:idx));
+else
+    error('Could not calculate FUR intergral because the plasma input does not extend until the midpoint of the brain scan');
+end
 
 end
