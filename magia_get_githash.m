556a4441e1767315ccde05891cd06b0457f9e179
Tomi Karjalainen <tomi.karjalainen@utu.fi>
2020-06-17 10:03:38 +0300

    Update magia_get_githash.m

--- magia_get_githash.m
+++ magia_get_githash.m
@@ -3,8 +3,13 @@ function githash = magia_get_githash()
 magia_path = getenv('MAGIA_PATH');
 
 githashfile = [magia_path '/.git/refs/heads/master'];
-fid = fopen(githashfile,'r');
-githash = fgetl(fid);
-fclose(fid);
+if(exist(githashfile,'file'))
+  fid = fopen(githashfile,'r');
+  githash = fgetl(fid);
+  fclose(fid);
+else
+  fprintf('Could not find the git hash file. Please download Magia using via command line using the command git clone https://github.com/tkkarjal/magia.git.);
+  githash = 'Not available';
+end
 
 end
