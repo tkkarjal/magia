f5d7a1433b35ad067270293b72b8b7c5c714d998
Tomi Karjalainen <tomi.karjalainen@utu.fi>
2021-04-07 20:52:46 +0300

    Update aivo_read_suvr_modeling_options.m

--- aivo/aivo_read_suvr_modeling_options.m
+++ aivo/aivo_read_suvr_modeling_options.m
@@ -5,7 +5,7 @@ if(isempty(conn))
     conn = aivo_connect();
 end
 
-cols = columns(conn,'megapet','aivo2','suvr');
+cols = aivo_columns(conn,'suvr');
 cols = setdiff(cols,'tracer','stable');
 M = length(cols);
 select_statement = 'SELECT * FROM "megabase"."aivo2".suvr';
