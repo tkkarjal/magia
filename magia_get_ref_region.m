78688e13c6d7a7cac0d7eff51c9ba7489e36b0f4
joupes <joupes@utu.fi>
2022-04-12 15:38:01 +0300

    Update magia_get_ref_region.m
    Changed PK11195 default ref-region to CER

--- magia_get_ref_region.m
+++ magia_get_ref_region.m
@@ -4,10 +4,10 @@ switch tracer
     case {'[11c]carfentanil' '[18f]dopa'}
         ref_region.label = 'OC';
         ref_region.codes = [1011 2011];
-    case {'[11c]raclopride','[11c]madam','[18f]spa-rq','[11c]pib','[11c]pbr28','[18f]cft','[11c]flb','[18f]fdg','[11c]ro15-4513'}
+    case {'[11c]raclopride','[11c]madam','[18f]spa-rq','[11c]pib','[11c]pbr28','[18f]cft','[11c]flb','[18f]fdg','[11c]ro15-4513','[11c]pk11195'}
         ref_region.label = 'CER';
         ref_region.codes = [8 47];
-    case {'[11c]pk11195' '[11c]tmsx'}
+    case {'[11c]tmsx'}
         ref_region.label = 'CAU';
         ref_region.codes = [11 50];
     case '[18f]fmpep-d2'
