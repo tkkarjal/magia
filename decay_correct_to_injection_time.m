a5ce412514ec887f8edbb271bc4aefb2dda62294
joupes <joupes@utu.fi>
2023-03-08 15:46:07 +0200

    decay factor fix
    fixed finding the correct decay factor based on the isotope

--- decay_correct_to_injection_time.m
+++ decay_correct_to_injection_time.m
@@ -11,11 +11,21 @@ V = spm_vol(uncorrected_pet_image);
 N = size(V,1);
 Vo = spm_file_split(V, odir);
 
-switch tracer
-    case {'[11c]carfentanil','[11c]raclopride','[11c]pib'}
-        half_life = 20.4; % minutes
-    case {'[18f]fdg','[18f]spa-rq','[18f]fmpep-d2'}
-        half_life = 109.8; % minutes
+isotope_idx = regexp(tracer,']');
+switch tracer(1:isotope_idx)
+    case {'[11c]'}, half_life = 20.4; % minutes
+    case {'[18f]'}, half_life = 109.8; % minutes
+    case {'[15o]'}, half_life = 2.05; % minutes
+    case {'[13n]'}, half_life = 9.98;
+    case {'[62cu]'}, half_life = 9.7;
+    case {'[64cu]'}, half_life = 762.018;
+    case {'[68ga]'}, half_life = 68.0;
+    case {'[68ge]'}, half_life = 396000;
+    case {'[76br]'}, half_life = 978;
+    case {'[82rb]'}, half_life = 1.25;
+    case {'[89zr]'}, half_life = 78.4;
+    case {'[124i]'}, half_life = 100.224;
+
     otherwise
         error;
 end
